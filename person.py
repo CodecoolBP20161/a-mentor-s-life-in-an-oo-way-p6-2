ccbc6e61db841b25d8882e7db27650355c8cac62
dsztanko <szt.dori@gmail.com>
2016-06-22 01:26:30 +0200

    handled empty string errors

--- person.py
+++ person.py
@@ -1,17 +1,21 @@
 class Person:
+    ''' This class represents a group of people. '''
+
     GENDER = ['male', 'female', 'notsure']
 
     def __init__(self, first_name, last_name, year_of_birth, gender, knowledge_level, energy_level):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.year_of_birth = year_of_birth
+        for arg in [first_name, last_name, year_of_birth, gender, knowledge_level, energy_level]:
+            if not arg:
+                raise ValueError('Empty parameter is forbidden.')
+            else:
+                self.first_name = first_name
+                self.last_name = last_name
+                self.year_of_birth = year_of_birth
 
-        if gender not in self.GENDER:
-            print('Not a valid gender.')
-            raise ValueError
-        else:
-            self.gender = gender
+                if gender not in self.GENDER:
+                    raise ValueError('Not a valid gender.')
+                else:
+                    self.gender = gender
 
-        self.year_of_birth = year_of_birth
-        self.knowledge_level = knowledge_level
-        self.energy_level = energy_level
+                self.knowledge_level = knowledge_level
+                self.energy_level = energy_level
