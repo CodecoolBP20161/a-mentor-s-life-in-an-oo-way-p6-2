090cf302e7a41c200ed2080f57ddedaded41abf1
dsztanko <szt.dori@gmail.com>
2016-06-22 11:19:06 +0200

    catches all the errors

--- person.py
+++ person.py
@@ -1,21 +1,30 @@
 class Person:
     ''' This class represents a group of people. '''
 
+    # acceptable gender types
     GENDER = ['male', 'female', 'notsure']
 
     def __init__(self, first_name, last_name, year_of_birth, gender, knowledge_level, energy_level):
-        for arg in [first_name, last_name, year_of_birth, gender, knowledge_level, energy_level]:
-            if not arg:
-                raise ValueError('Empty parameter is forbidden.')
-            else:
-                self.first_name = first_name
-                self.last_name = last_name
-                self.year_of_birth = year_of_birth
 
-                if gender not in self.GENDER:
-                    raise ValueError('Not a valid gender.')
-                else:
-                    self.gender = gender
+        try:
+            for arg in [first_name, last_name, year_of_birth, gender, knowledge_level, energy_level]:
+                if not arg:
+                    raise TypeError
 
-                self.knowledge_level = knowledge_level
-                self.energy_level = energy_level
+        except TypeError:
+            print('Empty parameter is forbidden.')
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.year_of_birth = year_of_birth
+
+        try:
+            if gender not in self.GENDER:
+                raise ValueError
+
+        except ValueError:
+            print('Not a valid gender.')
+
+        self.gender = gender
+        self.knowledge_level = knowledge_level
+        self.energy_level = energy_level
