215e9e3f6005d0345209c0db9cae4769dc72792e
KrisztaAnna <krisztina.baranyai@gmail.com>
2016-06-22 14:05:59 +0200

    mentor classmethod fix, args fix

--- mentor.py
+++ mentor.py
@@ -3,15 +3,17 @@ import csv
 
 
 class Mentor(Person):
-    def __init__(self, first_name, last_name, year_of_birth, gender, knowledge_level, energy_level,
-                 nickname=None, charisma=None):
-        super().__init__(first_name, last_name, year_of_birth, gender, knowledge_level, energy_level)
+    def __init__(self, nickname, charisma, *args):
+        super().__init__(*args)
         self.nickname = nickname
         self.charisma = charisma
 
-    def create_by_csv(path):
+    @classmethod
+    def create_by_csv(cls, path):
         with open(path, newline="") as csvfile:
             mentorreader = csv.reader(csvfile, delimiter=",")
             rows = list(mentorreader)
-            mentor_objects = [Mentor(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]) for row in rows]
+            mentor_objects = [Mentor(row[6], row[7], row[0], row[1], row[2], row[3], row[4], row[5]) for row in rows]
             return mentor_objects
+
+print(Mentor.create_by_csv("data/mentors.csv"))
